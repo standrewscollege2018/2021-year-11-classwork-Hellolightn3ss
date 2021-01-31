bad516e734d222433e5e638bebe5d34df6c345f5
Hellolightn3ss <68981667+Hellolightn3ss@users.noreply.github.com>
2021-02-01 09:33:33 +1300

    Create NAME.py

---
+++ NAME.py
@@ -0,0 +1,9 @@
+# type in name, respond with name. Creating an output using the input from users.
+
+name = input("what is your prefered name")
+
+print("Hello",name,"give me your mother's credit card")
+
+Freemoney = input("Input here")
+
+print("L get scammed idiot")
