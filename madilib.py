e0657be3301da3d3aa51fb2f57afe03e2e4b694a
Hellolightn3ss <68981667+Hellolightn3ss@users.noreply.github.com>
2021-02-02 14:08:45 +1300

    madid;odjdjfnlfkd

---
+++ madilib.py
@@ -0,0 +1,14 @@
+import time
+
+food = input("what is your prefered gender")
+
+good = input("what is your favourite genre")
+
+mood = input("what is the meaning of life")
+
+
+print("the year is 2096, the world wide convention for resource allocation has just ended, with New Zealand only being allocated half of last years resources, due to this the government has come to an agreement with the people of world 896457853#4567 in order to help their people survive.")
+
+time.sleep(15)
+
+print("The world is populated with only people that are", food,"and everyone obeys the rules of their religion CHICKEN which believes in", mood,"however the world is a world based around the", good, "genre.")
