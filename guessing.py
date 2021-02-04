381dcbe748e0e0bcd419542323ebd541f4da5a4d
Hellolightn3ss <68981667+Hellolightn3ss@users.noreply.github.com>
2021-02-04 15:11:48 +1300

    guessing

---
+++ guessing.py
@@ -0,0 +1,20 @@
+
+import random
+
+number = random.randint(1, 100)
+true = 1
+condition = 1
+
+while condition is true:
+    guess = int(input("what do you think the number is?"))
+    if guess > number:
+        print("hell no, too high up idiot")
+    if guess < number:
+        print("too low, nincompoop")
+    if guess == 69:
+        print("nice, but try again BAKA")
+    if guess == number:
+        print("finally dumbass")
+
+        number = random.randint(1, 100)
+
