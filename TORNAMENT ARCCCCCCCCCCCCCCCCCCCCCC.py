6a3b533db7edca3ab1f2d079d707b14a0151659c
Hellolightn3ss <68981667+Hellolightn3ss@users.noreply.github.com>
2021-02-09 15:32:14 +1300

    Create TORNAMENT ARCCCCCCCCCCCCCCCCCCCCCC.py

---
+++ TORNAMENT ARCCCCCCCCCCCCCCCCCCCCCC.py
@@ -0,0 +1,16 @@
+
+print("hello, and welcome to SAOistrash 2021, the ultimate fortnite dance battle driving battle royale POG CHAMP tournament")
+
+print("enter the name of the team competing.")
+Mainteam = input("input here")
+
+badteam = []
+sidecharacter = input("enter the other teams competing")
+if sidercharacter == "done":
+    teamnumber = (len(badteam))
+
+     else:
+         badteam.append(sidercharacter)
+
+
+
