c112ff686f9a795dc3f39438efeebb96d654b20d
Hellolightn3ss <68981667+Hellolightn3ss@users.noreply.github.com>
2021-02-16 14:23:42 +1300

    Update TORNAMENT ARCCCCCCCCCCCCCCCCCCCCCC.py

--- TORNAMENT ARCCCCCCCCCCCCCCCCCCCCCC.py
+++ TORNAMENT ARCCCCCCCCCCCCCCCCCCCCCC.py
@@ -13,32 +13,31 @@ while repeat == 0:
     sidecharacter = input("enter the other teams competing")
     if sidecharacter == "done":
          repeat = 1
-    while win is true:
+         while win is true:
 
-        teamnumber = (len(badteam))
-        print ("match",Mainteam,"against",badteam[X])
-        print ("input the scores of the teams")
+            teamnumber = (len(badteam))
+            print ("match",Mainteam,"against",badteam[X])
+            print ("input the scores of the teams")
 
-        if X == (len(badteam)):
-            win = 1
-            print("in this tornament", Mainteam,"has obtained",score,"points")
-        else:
-            Team1score = input(int("enter the interger score for",Mainteam,))
-            Team2score = input(int("enter the interger score for",badteam[X],))
-        if Team1score > Team2score:
-            print(Mainteam,"wins with a score of",Team1score,"against",badteam[X],"of",Team2score,)
-            X = X+1
-            score = score + 3
-        if Team2score > Team1score:
-            print(Badteam[X],"wins, with a score of",Team2score,"against",Mainteam,"of",Team1score)
-            X = X+1
-            score = score + 1
-        if Team2score == Team1score:
-             print("ladies and gentlemen it's a tie, with",badteam[X]," score of",Team2score,"against",Mainteam,"score of",Team1score)
-             X = X+1
-             score = score + 2
+            if X == (len(badteam)):
+                win = 1
+                print("in this tornament", Mainteam,"has obtained",score,"points")
+                else:
+                    Team1score = input(int("enter the integer score for your team"))
+                    Team2score = input(int("enter the integer score for the other team"))
+                    if Team1score > Team2score:
+                        print(Mainteam,"wins with a score of",Team1score,"against",badteam[X],"of",Team2score,)
+                        X = X+1
+                        score = score + 3
+                    if Team2score > Team1score:
+                        print(Badteam[X],"wins, with a score of",Team2score,"against",Mainteam,"of",Team1score)
+                        X = X+1
+                        score = score + 1
+                    if Team2score == Team1score:
+                        print("ladies and gentlemen it's a tie, with",badteam[X]," score of",Team2score,"against",Mainteam,"score of",Team1score)
+                        X = X+1
+                        score = score + 2
 
 
     else:
-         badteam.append(sidercharacter)
-
+         badteam.append(sidecharacter)
