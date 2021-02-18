3854b81461d71b24a882a16370a3fa8a63a34a0f
Hellolightn3ss <68981667+Hellolightn3ss@users.noreply.github.com>
2021-02-19 10:40:17 +1300

    Create bruh.py

---
+++ bruh.py
@@ -0,0 +1,24 @@
+
+print("welcome to the NOT A SCAM raffle of 2077")
+import random
+print("input the participents")
+true = 1
+win = 1
+people=[]
+while win is true:
+
+    add = input("")
+
+    if add == "end":
+        number = (len(people))
+        print =("the number of entries is",number,)
+        print =("today's prize is the ISSAC")
+        X = random.randint(0, number)
+        print = ("congradulations to",people[X-1],"for winning the ISSAC")
+
+
+    else :
+         people.append(add)
+
+
+
