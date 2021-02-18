66a6e2182810b54816a36c128405905a45935f95
Hellolightn3ss <68981667+Hellolightn3ss@users.noreply.github.com>
2021-02-18 16:05:30 +1300

    Update TORNAMENT ARCCCCCCCCCCCCCCCCCCCCCC.py

--- TORNAMENT ARCCCCCCCCCCCCCCCCCCCCCC.py
+++ TORNAMENT ARCCCCCCCCCCCCCCCCCCCCCC.py
@@ -3,9 +3,8 @@ print("hello, and welcome to SAOistrash 2021, the ultimate fortnite dance battle
 
 print("enter the name of the team competing.")
 Mainteam = input("input here")
-X=1
+X=0
 badteam = []
-badteam.append("BAKA")
 win = 0
 true =0
 repeat = 0
@@ -18,7 +17,7 @@ while repeat == 0:
 
             teamnumber = (len(badteam))
             print (badteam)
-            print ("match",Mainteam,"against",badteam[X-1])
+            print ("match",Mainteam,"against",badteam[X])
             print ("input the scores of the teams")
 
             if X == (len(badteam)):
@@ -30,15 +29,15 @@ while repeat == 0:
                 print("enter the integer score for the other team")
                 Team2score = input(int())
                 if Team1score > Team2score:
-                    print(Mainteam,"wins with a score of",Team1score,"against",badteam[X-1],"of",Team2score,)
+                    print(Mainteam,"wins with a score of",Team1score,"against",badteam[X],"of",Team2score,)
                     X = X+1
                     score = score + 3
                 if Team2score > Team1score:
-                    print(badteam[X-1],"wins, with a score of",Team2score,"against",Mainteam,"of",Team1score)
+                    print(badteam[X],"wins, with a score of",Team2score,"against",Mainteam,"of",Team1score)
                     X = X+1
                     score = score + 1
                 if Team2score == Team1score:
-                    print("ladies and gentlemen it's a tie, with",badteam[X-1]," score of",Team2score,"against",Mainteam,"score of",Team1score)
+                    print("ladies and gentlemen it's a tie, with",badteam[X]," score of",Team2score,"against",Mainteam,"score of",Team1score)
                     X = X+1
                     score = score + 2
     else:
