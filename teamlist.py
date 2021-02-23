a3b6018a6ba8e96f50b2e40ad038c133d89c41e7
Hellolightn3ss <68981667+Hellolightn3ss@users.noreply.github.com>
2021-02-23 15:27:09 +1300

    Create teamlist.py

---
+++ teamlist.py
@@ -0,0 +1,28 @@
+
+#team list task#
+print("Welcome to the EPIC free robux team")
+repeat = 1
+imperative = 0
+command = 0
+
+while repeat == 1:
+     print("1. Start the EPIC")
+     command = input("")
+     if command == 1:
+         print("1. Display full team list")
+         print("2. Add New Member to the Team")
+         print("3. Delete Member from the team")
+         print("4. Quit program")
+
+         imperative = input("")
+         if imperative = 4:
+             command = 0
+         if imperative = 3:
+             print("choose which member to be booted off the team, what a bunch of losers")
+
+
+
+
+
+     else:
+         print("error")
