a1613ecb08cf9dd7452907534f0fcf72e732c7fb
Hellolightn3ss <68981667+Hellolightn3ss@users.noreply.github.com>
2021-02-25 15:26:31 +1300

    Update teamlist.py

--- teamlist.py
+++ teamlist.py
@@ -1,9 +1,11 @@
 
 #team list task#
+import time
 print("Welcome to the EPIC free robux team")
 repeat = 1
 imperative = 0
 command = 0
+members = ["darth mander","isaac vader", "sam the mander man", "DOOM"]
 
 while repeat == 1:
      print("1. Start the EPIC")
@@ -15,10 +17,16 @@ while repeat == 1:
          print("4. Quit program")
 
          imperative = input("")
-         if imperative = 4:
+         if imperative == 4:
              command = 0
-         if imperative = 3:
+         if imperative == 3:
+             time.sleep(1)
              print("choose which member to be booted off the team, what a bunch of losers")
+             time.sleep(1)
+             print(members)
+             print("choose based on their number")
+             delete = int(input())
+             pop
 
 
 
