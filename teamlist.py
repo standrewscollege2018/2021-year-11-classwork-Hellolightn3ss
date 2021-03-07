c616f5c6601bde018c270cd40ca7fda424ec8670
Hellolightn3ss <68981667+Hellolightn3ss@users.noreply.github.com>
2021-03-08 08:47:19 +1300

    Update teamlist.py

--- teamlist.py
+++ teamlist.py
@@ -5,32 +5,48 @@ print("Welcome to the EPIC free robux team")
 repeat = 1
 imperative = 0
 command = 0
+number = 0
 members = ["darth mander","isaac vader", "sam the mander man", "DOOM"]
 
 while repeat == 1:
      print("1. Start the EPIC")
      command = input("")
-     if command == 1:
-         print("1. Display full team list")
-         print("2. Add New Member to the Team")
-         print("3. Delete Member from the team")
-         print("4. Quit program")
+     if command=="start":
+         if imperative == 0:
+            print("1. Display full team list")
+            print("2. Add New Member to the Team")
+            print("3. Delete Member from the team")
+            print("4. Quit program")
 
-         imperative = input("")
-         if imperative == 4:
-             command = 0
-         if imperative == 3:
+             number = input("")
              time.sleep(1)
-             print("choose which member to be booted off the team, what a bunch of losers")
-             time.sleep(1)
-             print(members)
-             print("choose based on their number")
-             delete = int(input())
-             pop
-
-
-
-
+             print("baka")
+            if number == 4:
+                command = 0
+            if number == 3:
+                time.sleep(1)
+                print("choose which member to be booted off the team, what a bunch of losers")
+                time.sleep(1)
+                print(members)
+                print("choose based on their number")
+                delete = int(input())
+                members.pop(delete)
+                imperative = 0
+            if number == 2:
+                print("imput the name of the new member")
+                new=input("")
+                time.sleep(1)
+                members.append(new)
+                imperative = 1
+            if number ==1:
+                print(members)
+                time.sleep(5)
+                print("do you wish to go back, if yes do Y if no do N")
+                leave=input("")
+                if leave == Y:
+                    imperative=0
+                if leave == N:
+                    imperative=1
 
      else:
          print("error")
